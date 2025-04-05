1d66876e5951a7469db203c5d94972c9b3c1e4cb
ZYTON <priyanshu7pandey@gmail.com>
2025-04-05 23:43:55 +0530

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        double num1;
+        double num2;
+        char operator;
+        double result = 0;
+        boolean validOperator = true;
+
+        System.out.print("Enter the first number: ");
+        num1 = scanner.nextDouble();
+
+        System.out.print("Enter an operator (+, -, *, /, ^): ");
+        operator = scanner.next().charAt(0);
+
+        System.out.print("Enter the second number: ");
+        num2 = scanner.nextDouble();
+
+        switch (operator){
+            case '+' -> result = num1 + num2;
+            case '-' -> result = num1 - num2;
+            case '*' -> result = num1 * num2;
+            case '/' -> {
+                if(num2 == 0){
+                    System.out.println("Cannot divide by zero");
+                    validOperator = false;
+                }
+                else{
+                    result = num1 / num2;
+                }
+            }
+            case '^' -> result = Math.pow(num1,num2);
+            default -> {
+                System.out.println("Invalid Operator!");
+                validOperator = false;
+            }
+        }
+        if(validOperator) {
+            System.out.printf("ANS: %.2f", result);
+        }
+        scanner.close();
+
+    }
+}
